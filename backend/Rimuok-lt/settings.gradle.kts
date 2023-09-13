5ed12088de347220d5d0e5f056de20678ad66b79
Maximas <maksimas.pavlenko@gmail.com>
2023-09-13 21:51:17 +0300

    Switched gradle to maven

--- backend/Rimuok-lt/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "Rimuok-lt"
--- backend/Rimuok-lt/src/main/java/lt/rimuok/Main.java
+++ backend/Rimuok-lt/src/main/java/lt/rimuok/Main.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) {;
 		System.out.println("Привет! ąčęėįšųū");
 		SpringApplication.run(Main.class, args);
 	}
